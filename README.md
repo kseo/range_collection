0a57d14b5d964d3e6259c17abf969a1037b3e06e
Kwang Yul Seo <kwangyul.seo@gmail.com>
2015-11-02 10:46:42 +0900

    Initial import.

---
+++ README.md
@@ -0,0 +1,19 @@
+# range_collection
+
+A library for Dart developers. It is awesome.
+
+## Usage
+
+A simple usage example:
+
+    import 'package:range_collection/range_collection.dart';
+
+    main() {
+      var awesome = new Awesome();
+    }
+
+## Features and bugs
+
+Please file feature requests and bugs at the [issue tracker][tracker].
+
+[tracker]: http://example.com/issues/replaceme
---
+++ lib/range_collection.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The range_collection library.
+///
+/// This is an awesome library. More dartdocs go here.
+library range_collection;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/range_collection_base.dart';
